4095cb95d678c0b2b85c35cac965dbc5d3791936
Niloo Ravaei <nilooravaei@gmail.com>
2018-11-26 18:23:51 -0500

    update change calculator

--- week0/change-calculator.js
+++ week0/change-calculator.js
@@ -1,49 +1,19 @@
 var calculateChange = function(total, cash) {
-  var changeBack = {}
-  var change = cash - total;
+    var returnValue = cash - total;
+    var change = {};
+    var coinTypes = [2000, 1000, 500, 200, 100, 25, 10, 5, 1];
+    var denominations = ["twenty", "ten", "five", "twoDollar", "dollar", "quarter", "dime", "nickel", "penny"];
+    var amount;
 
-  while (change > 0) {
-    switch (true) {
-      case (change >= 2000):
-        changeBack["twenty"] = (changeBack["twenty"] + 1) || 1;
-        change -= 2000;
-        break;
-      case (change >= 1000):
-        changeBack["ten"] = (changeBack["ten"] + 1) || 1;
-        change -= 1000;
-        break;
-      case (change >= 500):
-        changeBack["five"] = (changeBack["five"] + 1) || 1;
-        change -= 500;
-        break;
-      case (change >= 200):
-        changeBack["twoDollar"] = (changeBack["twoDollar"] + 1) || 1;
-        change -= 200;
-        break;
-      case (change >= 100):
-        changeBack["dollar"] = (changeBack["dollar"] + 1) || 1;
-        change -= 100;
-        break;
-      case (change >= 25):
-        changeBack["quarter"] = (changeBack["quarter"] + 1) || 1;
-        change -= 25;
-        break;
-      case (change >= 10):
-        changeBack["dime"] = (changeBack["dime"] + 1) || 1;
-        change -= 10;
-        break;
-      case (change >= 5):
-        changeBack["nickle"] = (changeBack["nickle"] + 1) || 1;
-        change -= 5;
-        break;
-      case (change >= 1):
-        changeBack["penny"] = (changeBack["penny"] + 1) || 1;
-        change -= 1;
-        break;
+    for (var i = 0; i < coinTypes.length; i++) {
+        amount = Math.floor(returnValue/coinTypes[i]);
+        if (amount > 0) {
+            change[denominations[i]] = amount;
+            returnValue = returnValue%coinTypes[i];
+        }
     }
-  }
 
-  return changeBack;
+    return change;
 };
 
 console.log(calculateChange(1787, 2000));
